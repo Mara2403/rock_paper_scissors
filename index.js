181a6bda838dadc6b2c484dc7c922af762485dc6
Mara <125482894+Mara2403@users.noreply.github.com>
2023-07-17 11:43:31 +0200

    add modified singleRound() function

--- index.js
+++ index.js
@@ -41,129 +41,59 @@ function winner(computerSelection, userSelection) {
 
 let userPoints = 0;
 
-function singleRound() {
+let rockButton = document.getElementById('btnRock');
+let paperButton = document.getElementById('btnPaper');
+let scissorsButton = document.getElementById('btnScissors');
+  
+rockButton.addEventListener('click', singleRound);
+paperButton.addEventListener('click', singleRound);
+scissorsButton.addEventListener('click', singleRound);
+    
 
-  //computer choice//
-  const computerSelection = getComputerChoice(); /*am I calling a function here?*/
-  console.log("Computer made its choice.");
-  console.log(computerSelection); //to be removed later to not see what it chose//
+function singleRound(e) {
 
   //user choice//
-  /*let userSelection = prompt('Choose one: rock, paper or scissors');
+  const userSelection = e.target.value.toLowerCase();
+
+  //computer choice//
+  const computerSelection = getComputerChoice();
+  console.log("Computer chose - " + computerSelection);
+  
+
+  //user choice, no UI version//
+  /*const userSelection = prompt('Choose one: rock, paper or scissors');
   userSelection = userSelection.toLowerCase();*/
 
-
-  function userMadeChoice() {
-    let rockButton = document.getElementById('btnRock');
-    let paperButton = document.getElementById('btnPaper');
-    let scissorsButton = document.getElementById('btnScissors');
   
-    rockButton.addEventListener('click', userChoice);
-    paperButton.addEventListener('click', userChoice);
-    scissorsButton.addEventListener('click', userChoice);
-  
-    function userChoice(e) {
-    console.log(e.target.value.toLowerCase());
-  }
-  }
-  
-  
-  let userSelection = userMadeChoice();
-  console.log(userSelection);
-
   //winner of this round is://
   if ((userSelection == 'rock') || (userSelection == 'paper') || (userSelection == 'scissors')) {
-  console.log("You've chosen - " + userSelection);
-  console.log(winner(computerSelection, userSelection));
+  console.log("You chose - " + userSelection);
+  console.log(winner(computerSelection, userSelection));}
 
-
-    if (winner(computerSelection, userSelection) == 'You won! Rock beats scissors.' 
-    || winner(computerSelection, userSelection) == 'You won! Paper beats rock.'
-    || winner(computerSelection, userSelection) == 'You won! Scissors beats paper.') {
-    userPoints = userPoints + 1;
-    console.log('User points: ' + userPoints);
-    return userPoints;
+  if (winner(computerSelection, userSelection) == 'You won! Rock beats scissors.' 
+  || winner(computerSelection, userSelection) == 'You won! Paper beats rock.'
+  || winner(computerSelection, userSelection) == 'You won! Scissors beats paper.') {
+  userPoints = userPoints + 1;
+  console.log('User points: ' + userPoints);
   }
   else {
     console.log('User points: ' + userPoints);
   };
-  } else {
-     console.log('Try again.')
-  }
 
+  if (userPoints >= 3) {
+      console.log("YOU WON!")
+    return
+    };
+  
+
+
+let userChoiceText = document.getElementById('players-choice');
+userChoiceText.textContent = "You chose - " + userSelection + ", " + "computer chose - " + computerSelection + ".";
+let winnerOfTheRound = document.querySelector("h2");
+winnerOfTheRound.textContent = winner(computerSelection, userSelection);
+
+  
 };
 
 
-function game() {
-  
-singleRound();
-console.log(userPoints);
-singleRound();
-console.log(userPoints);
-singleRound();
-console.log(userPoints);
-if (userPoints == 3) {
-    console.log('You WON! Congratulations!');
-    return;
-  } 
-singleRound();
-console.log(userPoints);
-if (userPoints == 3) {
-  console.log('You WON! Congratulations!');
-  return;
-} 
-singleRound();
-console.log(userPoints);
-if (userPoints == 3) {
-  console.log('You WON! Congratulations!');
-  return;
-} 
-
-}
-
-
-game();
-let userChoiceText = document.getElementById('user-choice');
-
-
-
-/* Comment out - trying something else
-
-let rockButton = document.getElementById('btnRock');
-let paperButton = document.getElementById('btnPaper');
-let scissorsButton = document.getElementById('btnScissors');
-
-rockButton.addEventListener('click', userChoice);
-paperButton.addEventListener('click', userChoice);
-scissorsButton.addEventListener('click', userChoice);
-
-
-
-function userChoice(e) {
-  //console.log(e.target.value);
-  userChoiceText.textContent = "You chose " + e.target.value;
-  let userChose = e.target.value;
-  console.log(userChose);
-  return userChose;
-}
-
-*/
-/* Putting it in the singleRound()
-function userMadeChoice() {
-  let rockButton = document.getElementById('btnRock');
-  let paperButton = document.getElementById('btnPaper');
-  let scissorsButton = document.getElementById('btnScissors');
-
-  rockButton.addEventListener('click', userChoice);
-  paperButton.addEventListener('click', userChoice);
-  scissorsButton.addEventListener('click', userChoice);
-
-  function userChoice(e) {
-  console.log(e.target.value.toLowerCase());
-}
-}
-
-
-let userSelection = userMadeChoice();
-console.log(userSelection);
-*/
+/* discord comment: In my opinion you have too many functions. If you read assignments more carefully and plan what you do on a paper, you can see you handle just with `getComputerChoice() {//Write your code for computer choise}` function, `playRound(computerSelection, userSelection) {//Write your conditions how to know who wins}` function and `game() {//Write your code to see who wins the game(5 rounds)}` and maybe `addEventListener()` if you need it.*/
